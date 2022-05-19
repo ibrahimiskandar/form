5a86b05a73c94972ce119c06431ac877b3dcc5af
ibrahimiskandar <100687451+ibrahimiskandar@users.noreply.github.com>
2022-05-20 03:10:11 +0400

    Add files via upload

---
+++ validation/action.php
@@ -0,0 +1,10 @@
+<html>
+<body>
+
+First Name: <?php echo $_POST["firstname"]; ?><br>
+Last Name:<?php echo $_POST["lasttname"]; ?><br>
+Email address: <?php echo $_POST["email"]; ?><br>
+Age: <?php echo $_POST["age"]; ?>
+
+</body>
+</html>
